0ab465d7831986c82ab991831a7b73b6759160f8
Jesus <jesusmelara12@gmail.com>
2020-09-21 20:39:48 -0600

    Expo 2020

---
+++ modulos/creditos.php
@@ -0,0 +1,15 @@
+<?php
+echo "<font size=8> <b> INTEGRANTES:</b> </font>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<font size=6><b>✯ Jesús Alejandro Melara Figueroa</b> </font>";
+echo "<br>";
+echo "<font size=6><b>✯ Keiry Maidena Zaldaña Orellana</b></font>";
+echo "<br>";
+echo "<font size=6><b>✯ Nahúm Elíseo Chávez Avalos</b></font>";
+echo "<br>";
+echo "<font size=6><b>✯ Obed Alberto Portillo Chavez</b></font>";
+echo "<br>";
+echo "<font size=6><b>✯ Jorge Eduardo Gómez Silva</b></font>";
+?>
